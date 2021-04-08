a23b078674345753d9715f26fcbbb5ffa49e534c
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2021-04-07 17:26:31 -0700

    step 40 or 70 welp

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ let doorImage1 = document.getElementById('door1')
 let doorImage2 = document.getElementById('door2')
 let doorImage3 = document.getElementById('door3')
 
+let numClosedDoors = 3;
+
+let openDoor1 = botDoorPath
+let openDoor2 = beachDoorPath
+let openDoor3 = spaceDoorPath
+
+randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random() * numClosedDoors)
+        if(choreDoor === 0) {
+            openDoor1 = botDoorPath;
+            openDoor2 = openDoor3;
+            openDoor3 = openDoor2;
+        } else if (choreDoor === 1) {
+            openDoor2 = botDoorPath;
+            openDoor1 = openDoor3;
+            openDoor3 = openDoor1;
+        } else if (choreDoor === 2) {
+            openDoor3 = botDoorPath;
+            openDoor2 = openDoor1;
+            openDoor1 = openDoor2;
+        }
+}
+
 
 let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
 doorImage1.onclick = () => {
